2cdf15dff23a52e7c7ecb2dac67966a97177599d
井上 航太 <inoue@MacBook-Pro-2.local>
2017-09-10 17:36:43 +0900

    api-endpointを変更

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@ const instance = axios.create({
 });
 
 export default {
-  fetchArticles(page = 1, size = 100, query) {
-    return instance.get(`api/v2/items?page=${page}&per_page=${size}&query=${query}`)
+  fetchArticles(page = 1, per_page = 100, query) {
+    return instance.get('api/v2/items', { page, per_page, query })
   }
 }
