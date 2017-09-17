572866054b70fdd3d2f718715a0f5d34564d9c91
竹内 裕紀 (たんめん) <yuki.takeuchi@bizreach.co.jp>
2017-09-17 20:21:06 +0900

    変数名変更

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -4,9 +4,9 @@ import { actions } from '../../type.root';
 const defaultState = { login: false, token: null };
 
 export default handleActions({
-  [actions.login]: (state, actions) => ({
+  [actions.login]: (state, action) => ({
     ...state,
     login: true,
-    token: actions.payload || null
+    token: action.payload || null
   })
 }, defaultState);
