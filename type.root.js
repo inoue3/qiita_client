19855056a84e6d34794891d97265f184b6ce112b
井上 航太 <inoue@MacBook-Pro-2.local>
2017-09-25 16:11:43 +0900

    レビューに対応

--- type.root.js
+++ type.root.js
@@ -1,17 +1,6 @@
 import { createActions } from 'redux-actions'
 
-export const LOGIN =  {
-  login
-} = createActions('LOGIN');
-
-export const GET_POPULAR_ARTICLES =  {
-  getPopularArticles
-} = createActions('GET_POPULAR_ARTICLES');
-
-export const FETCH_NEW_ARTICLES =  {
-  fetchNewArticles
-} = createActions('FETCH_NEW_ARTICLES');
-
-export const SEARCH_ARTICLES =  {
-  searchArticles
-} = createActions('SEARCH_ARTICLES');
+export const LOGIN = createActions('LOGIN');
+export const GET_POPULAR_ARTICLES = createActions('GET_POPULAR_ARTICLES');
+export const FETCH_NEW_ARTICLES = createActions('FETCH_NEW_ARTICLES');
+export const SEARCH_ARTICLES = createActions('SEARCH_ARTICLES');
